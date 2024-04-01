fcec8f73c1d376b412487de4e92e5f55ccb8dbfb
jeanchiang <jeanchiang1221@gmail.com>
2024-04-01 14:56:49 +0800

    測試3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const requestListener = (req, res)=>{
         todos.splice(index,1);
         res.writeHead(200,headers);
         res.write(JSON.stringify({
-            "status": "success(Delete)",
+            "status": "success(Delete!)",
             "data": todos,
         }));
         res.end();
